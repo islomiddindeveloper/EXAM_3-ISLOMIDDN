aedaaef0fa364b3fb38b3ceb453fad7f356fb6c4
islomiddindeveloper <islomiddincoder@gmail.com>
2023-03-05 17:51:42 +0500

    exam-3

---
+++ js/main.js
@@ -0,0 +1,23 @@
+includeHTML();
+
+
+
+
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.opacity = 1;
+  } else {
+    backtop.style.opacity = 0;
+  }
+}
+
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
